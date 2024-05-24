4750b282a4a5438834dc83f22f59efe286c8659f
0xAA55 <weixiaobao213@qq.com>
2024-05-24 16:42:48 +0800

    调试字体库

--- font.hpp
+++ font.hpp
@@ -6,6 +6,6 @@
 
 namespace TVOS
 {
-	bool GetGlyphSize(uint32_t Unicode, int& Width, int& Height);
-	bool ExtractGlyph(ImageBlock& ImgOut, uint32_t Unicode, uint32_t color1, uint32_t color2);
+	bool GetGlyphSize(uint32_t Unicode, int& Width, int& Height, bool Verbose);
+	bool ExtractGlyph(ImageBlock& ImgOut, uint32_t Unicode, uint32_t color1, uint32_t color2, bool Verbose);
 }
