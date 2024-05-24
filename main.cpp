58e548401c5f3751772c5a47b11a9209253ea60b
0xAA55 <weixiaobao213@qq.com>
2024-05-24 12:54:04 +0800

    尝试写入像素。

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 
+#include "graphics.hpp"
+
 #include <cstdio>
 #include <cstdlib>
-
-#include "graphics.hpp"
+#include <iostream>
 
 using namespace TVOS;
 
@@ -10,10 +11,14 @@ int main(int argc, char** argv, char** envp)
 {
 	auto FB = Graphics(true);
 
+	int putx = 0;
+	int puty = 0;
 	for(int i = 0; i < 10000; i++)
 	{
-		FB.PutPixel(std::rand() % FB.GetWidth(), std::rand() % FB.GetHeight(),
-			MakeColor(std::rand() % 256, std::rand() % 256, std::rand() % 256));
+		std::cout << "[INFO] Putting pixel at " << putx << "x" << puty << "\n";
+		FB.PutPixel(putx, puty, MakeColor(255, 255, 255));
+		putx = std::rand() % FB.GetWidth();
+		puty = std::rand() % FB.GetHeight();
 	}
 
 	return 0;
