9cab81b98ca5578845d6bcdbb4ad255660653240
0xAA55 <weixiaobao213@qq.com>
2024-05-24 09:32:13 +0800

    修复头文件拼错的问题。

--- utf.hpp
+++ utf.hpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include <cstdint>
 #include <cstddef>
-#include <stdxecept>
+#include <stdexcept>
 
 namespace UTF
 {
@@ -12,7 +12,7 @@ namespace UTF
 	{
 	public:
 		UtfConversionError(const std::string& what) noexcept;
-	}
+	};
 
 	std::vector<uint32_t> Utf8_to_Utf32(const std::string& Utf8s);
 	std::string Utf32_to_Utf8(const std::vector<uint32_t>& Utf32s);
