3b099f14ded18fd940cc04567b4ee1a81fd2d91d
0xAA55 <weixiaobao213@qq.com>
2024-05-25 15:32:11 +0800

    修改测试文本。

--- tvos_windebug/tvos.cpp
+++ tvos_windebug/tvos.cpp
@@ -10,7 +10,7 @@ int main()
 	auto FB = MyTestApp(true);
 	
 	FB.ClearScreen(0xFFFFFFFF);
-	FB.DrawText(200, 200, "啊!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ", true, 0xFFFF0000);
+	FB.DrawText(200, 200, "测试文本绘制", true, 0xFFFF0000);
 
 	FB.RefreshFB();
 	FB.MainLoop();
