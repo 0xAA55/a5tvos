6bc16046edae7e168f2c07a1cd37bde3ce90d2ab
0xAA55 <weixiaobao213@qq.com>
2024-05-25 20:01:32 +0800

    改为绿色调试字体。

--- tvos_windebug/tvos.cpp
+++ tvos_windebug/tvos.cpp
@@ -10,7 +10,7 @@ int main()
 	auto FB = MyTestApp(true);
 	
 	FB.ClearScreen(0xFFFFFFFF);
-	FB.DrawText(200, 200, "测试文本绘制", true, 0xFFFF0000);
+	FB.DrawText(200, 200, "测试文本绘制", true, 0xFF00FF00);
 
 	FB.RefreshFB();
 	FB.MainLoop();
