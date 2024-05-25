a25b0b5111f8c64731a18ed3bf18de0c028ad909
0xAA55 <weixiaobao213@qq.com>
2024-05-25 14:00:04 +0800

    添加测试程序。

--- tvos_windebug/tvos.cpp
+++ tvos_windebug/tvos.cpp
@@ -8,15 +8,47 @@ using namespace TVOS;
 using namespace DIBWin;
 
 class MyTestApp :
-    public Window,
-    public Graphics
+	public Window,
+	public Graphics
 {
 public:
-    MyTestApp();
-    MyTestApp(int Width, int Height);
+	MyTestApp();
+	MyTestApp(int Width, int Height);
+	MyTestApp(int Width, int Height, bool Verbose);
+
+	void RefreshFB();
 };
 
 int main()
 {
-    
+	auto FB = MyTestApp();
+	
+	FB.FillRectXor(200, 200, 800 - 200, 480 - 200);
+	FB.DrawText(200, 200, "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ", false, 0);
+
+	FB.RefreshFB();
+	FB.MainLoop();
+}
+
+MyTestApp::MyTestApp() :
+	MyTestApp(800, 480)
+{
+}
+
+MyTestApp::MyTestApp(int Width, int Height) :
+	Window(Width, Height, "TVOS"),
+	Graphics(nullptr, Width, Height, Verbose)
+{
+}
+
+MyTestApp::MyTestApp(int Width, int Height, bool Verbose) :
+	Window(Width, Height, "TVOS"),
+	Graphics(nullptr, Width, Height, Verbose)
+{
+}
+
+void MyTestApp::RefreshFB()
+{
+	memcpy(GetFBPtr(), &BackBuffer->Pixels[0], BackBuffer->GetStride() * BackBuffer->h);
+	Window::RefreshFB();
 }
