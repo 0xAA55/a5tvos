d1f56a711387282c62d35c23bf20425293fdfb97
0xAA55 <weixiaobao213@qq.com>
2024-05-26 23:19:27 +0800

    实现 `SetMode()`

--- gpio.cpp
+++ gpio.cpp
@@ -25,3 +25,28 @@ void GPIO_PeriphType::SetModeDisabled(int Port)
 {
 	SetMode(Port, 7);
 }
+
+void GPIO_PeriphType::SetMode(int Port, uint32_t Mode)
+{
+	int Shift;
+	if (Port < 8)
+	{
+		Shift = Port * 4;
+		CFG0 = CFG0 & (~(7 << Shift)) | (Mode << Shift);
+	}
+	else if (Port < 16)
+	{
+		Shift = (Port - 8) * 4;
+		CFG1 = CFG1 & (~(7 << Shift)) | (Mode << Shift);
+	}
+	else if (Port < 24)
+	{
+		Shift = (Port - 16) * 4;
+		CFG2 = CFG2 & (~(7 << Shift)) | (Mode << Shift);
+	}
+	else 
+	{
+		Shift = (Port - 24) * 4;
+		CFG3 = CFG3 & (~(7 << Shift)) | (Mode << Shift);
+	}
+}
