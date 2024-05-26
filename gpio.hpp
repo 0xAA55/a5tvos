d0bb3c29faf20d722c66ca279f16d225be25be93
0xAA55 <weixiaobao213@qq.com>
2024-05-26 22:44:53 +0800

    尝试实现 GPIO

---
+++ gpio.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <cstdint>
+
+struct GPIO_PeriphType
+{
+	volatile uint32_t CFG0;
+	volatile uint32_t CFG1;
+	volatile uint32_t CFG2;
+	volatile uint32_t CFG3;
+	volatile uint32_t DATA;
+	volatile uint32_t DRV0;
+	volatile uint32_t DRV1;
+	volatile uint32_t PUL0;
+	volatile uint32_t PUL1;
+
+	bool ReadBit(int Port) const;
+	void WriteBit(int Port);
+};
+
+const GPIO_PeriphType* GPIO_Periph = reinterpret_cast<GPIO_PeriphType*>(0x01C20800);
+
+enum GPIO_GroupEnumType
+{
+	GPIO_A = 0,
+	GPIO_B = 0,
+	GPIO_C = 0,
+	GPIO_D = 0,
+	GPIO_E = 0,
+	GPIO_F = 0,
+};
