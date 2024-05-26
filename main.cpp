f881486ad9aba4d166bd6da6b355bf9681d4900c
0xAA55 <weixiaobao213@qq.com>
2024-05-26 14:57:02 +0800

    尝试制作 splash 屏幕。

--- main.cpp
+++ main.cpp
@@ -4,23 +4,55 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <filesystem>
+
+#include <chrono>
+#include <thread>
 
 using namespace TVOS;
 
 int main(int argc, char** argv, char** envp)
 {
-	// auto FB = Graphics(480, 272, false);
-	auto FB = Graphics(false);
+	const ResoW = 480;
+	const ResoH = 272;
+	auto FB = Graphics(ResoW, ResoH, false);
 
-	for(int i = 0; i < 50; i++)
+	int SplashTextW;
+	int SplashTextH;
+	FB.GetTextMetrics("请插入 SD 卡。", SplashTextW, SplashTextH);
+	while (true);
 	{
-		int putx = std::rand() % FB.GetWidth();
-		int puty = std::rand() % FB.GetHeight();
-		// FB.DrawText(putx, puty, "测试文本绘制", true, MakeColor(std::rand() % 256, std::rand() % 256, std::rand() % 256));
-		FB.DrawText(putx, puty, "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ", false, 0);
+		int SplashX = 0;
+		int SplashY = 0;
+		int SplashPadding = 20;
+		int SplashMoveX = 20;
+		int SplashMoveY = 20;
+		while (!std::filesystem::exists(std::filesystem::path("/dev/mmcblk0p1")))
+		{
+			int SplashR = SplashX + SplashTextW + SplashPadding * 2 - 1;
+			int SplashB = SplashY + SplashTextH + SplashPadding * 2 - 1;
+			if (SplashR + 1 >= ResoW)
+			{
+				SplashR = ResoW - 1;
+				SplashX = ResoW - SplashTextW - SplashPadding * 2 - 1;
+				SplashMoveX = -20;
+			}
+			if (SplashB + 1 >= ResoH)
+			{
+				SplashB = ResoH - 1;
+				SplashY = ResoH - SplashTextH - SplashPadding * 2 - 1;
+				SplashMoveY = -20;
+			}
+			if (SplashX < 0)
+			{
+				SplashX = 0;
+			}
+			FB.ClearScreen(0);
+			FB.FillRect(SplashX, SplashY, SplashR, SplashB, 0xFFA0A0A0);
+			FB.DrawText(SplashX + SplashPadding, SplashY + SplashPadding, "请插入 SD 卡。", true, 0xFFFFFFFF);
+			std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		}
 	}
-	FB.ClearScreen(0);
-	FB.DrawText(200, 100, "测试文本绘制", false, 0);
 
 	return 0;
 }
