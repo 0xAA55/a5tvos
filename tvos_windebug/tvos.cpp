284fdcbffefd7547f7405f477c7fbbdc646b8354
0xAA55 <weixiaobao213@qq.com>
2024-05-27 12:38:41 +0800

    测试 GUI 的表现。

--- tvos_windebug/tvos.cpp
+++ tvos_windebug/tvos.cpp
@@ -9,19 +9,21 @@ using namespace TVOS;
 int main()
 {
 	auto FB = MyTestApp(true);
+	auto GUI = UIElementBase(FB, "root");
+	FB.ClearScreen(0);
+
+	GUI.XMargin = 10;
+	GUI.YMargin = 10;
+	GUI.XBorder = 2;
+	GUI.YBorder = 2;
+	GUI.BorderColor = 0xFFFFFFFF;
+	GUI.FillColor = 0;
 	
 
 
 
 
-
-
-
-
-	FB.ClearScreen(0);
-
-
-	FB.DrawText(200, 200, "请插入 SD 卡", true, 0xFFFFFFFF);
+	GUI.Render(0, 0, FB.GetWidth(), FB.GetHeight());
 
 	FB.RefreshFB();
 	FB.MainLoop();
