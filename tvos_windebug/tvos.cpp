b4154e19ee0f97992f9a0e8676f8cdef9061b2fa
0xAA55 <weixiaobao213@qq.com>
2024-05-27 17:03:55 +0800

    测试生成一百个控件，看它的排版是否合理。

--- tvos_windebug/tvos.cpp
+++ tvos_windebug/tvos.cpp
@@ -18,11 +18,29 @@ int main()
 	GUI.YBorder = 2;
 	GUI.BorderColor = 0xFFFFFFFF;
 	GUI.FillColor = 0;
-	
+	GUI.Transparent = true;
+	GUI.ExpandToParentX = true;
+	GUI.ExpandToParentY = true;
 
+	for (int i = 0; i < 100; i++)
+	{
+		auto Sub = std::make_shared<UIElementLabel>(FB, std::string("test") + std::to_string(i));
+		GUI.InsertElement(Sub);
+		Sub->XMargin = 2;
+		Sub->YMargin = 2;
+		Sub->XBorder = 1;
+		Sub->YBorder = 1;
+		Sub->XPadding = 2;
+		Sub->YPadding = 2;
+		Sub->BorderColor = 0xFFFFFFFF;
+		// Sub->ExpandToParentX = true;
+		// Sub->LineBreak = true;
+		Sub->Transparent = true;
+		Sub->SetCaption(Sub->GetName());
 
+	}
 
-
+	GUI.ArrangeElements(0, 0, FB.GetWidth(), FB.GetHeight());
 	GUI.Render(0, 0, FB.GetWidth(), FB.GetHeight());
 
 	FB.RefreshFB();
