882b7feabbc52738d1f986222f30aaf79cb50926
0xAA55 <weixiaobao213@qq.com>
2024-05-28 09:01:02 +0800

    实现自动检测是否接入了 SD 卡，然后进行 `mount()` / `umount()` 操作。

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 
 #include "graphics.hpp"
 #include "gui.hpp"
+#include "gpio.hpp"
+
+#include <sys/mount.h>
 
 #include <cstdio>
 #include <cstdlib>
@@ -16,6 +19,11 @@ int main(int argc, char** argv, char** envp)
 {
 	const int ResoW = 480;
 	const int ResoH = 272;
+
+	bool Mounted = false;
+
+	WriteGPIOE(0, true);
+
 	auto FB = Graphics(ResoW, ResoH, false);
 	auto GUI = UIElementBase(FB, "root");
 	FB.ClearScreen(0);
@@ -49,12 +57,71 @@ int main(int argc, char** argv, char** envp)
 		Sub->SetCaption(Sub->GetName());
 	}
 
-	GUI.ArrangeElements(0, 0, FB.GetWidth(), FB.GetHeight());
-	GUI.Render();
 
 	while (true);
 	{
-		
+		if (!std::filesystem::exists(std::filesystem::path("/dev/mmcblk0p1")))
+		{
+			if (Mounted)
+			{
+				if (umount("/mnt/sdcard") == 0)
+				{
+					Mounted = false;
+				}
+			}
+			GUI.ClearElements();
+
+			auto Sub = std::make_shared<UIElementLabel>(FB, "Title");
+			GUI.InsertElement(Sub);
+			Sub->XMargin = 0;
+			Sub->YMargin = 0;
+			Sub->XBorder = 0;
+			Sub->YBorder = 1;
+			Sub->XPadding = 2;
+			Sub->YPadding = 2;
+			Sub->BorderColor = 0xFFFFFFFF;
+			Sub->ExpandToParentX = true;
+			Sub->LineBreak = false;
+			Sub->Transparent = true;
+			Sub->SetCaption("A5-MiniTV 小电视");
+
+			auto Prompt = std::make_shared<UIElementLabel>(FB, "Prompt");
+			GUI.InsertElement(Prompt);
+			Prompt->XMargin = 0;
+			Prompt->YMargin = 0;
+			Prompt->XBorder = 0;
+			Prompt->YBorder = 0;
+			Prompt->XPadding = 0;
+			Prompt->YPadding = 0;
+			Prompt->BorderColor = 0xFFFFFFFF;
+			Prompt->ExpandToParentX = true;
+			Prompt->ExpandToParentY = true;
+			Prompt->LineBreak = false;
+			Prompt->Transparent = true;
+			Prompt->SetCaption("请插入 SD 卡");
+		}
+		else
+		{
+			auto SDCardPath = std::filesystem::path("/mnt/sdcard");
+			if (!std::filesystem::exists(SDCardPath))
+			{
+				std::filesystem::create_directories(SDCardPath);
+			}
+
+			if (!Mounted)
+			{
+				if (mount("/dev/mmcblk0p1", "/mnt/sdcard", "vfat", 0, "defaults,nofail") == 0)
+				{
+					Mounted = true;
+				}
+			}
+
+			GUI.ClearElements();
+			
+		}
+
+		GUI.ArrangeElements(0, 0, FB.GetWidth(), FB.GetHeight());
+		GUI.Render();
 		std::this_thread::sleep_for(std::chrono::milliseconds(500));
 	}
 
