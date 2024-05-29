c9d29bfcb53ede3b577eacd2426f2e6a899039a9
0xAA55 <weixiaobao213@qq.com>
2024-05-29 14:49:32 +0800

    删除旧注释代码。

--- graphics.cpp
+++ graphics.cpp
@@ -322,10 +322,6 @@ namespace TVOS
 
 	void Graphics::WriteData(const uint32_t* pixels, int Count)
 	{
-		// if (Verbose)
-		// {
-		// 	std::cout << "[INFO] Writting " << Count << " pixels from pointer " << std::hex << pixels << std::dec << ".\n";
-		// }
 		if (BackBufferMode)
 		{
 			auto* Buffer = BackBuffer.get();
